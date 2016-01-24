9dae99a3a0a5a440edf2e04d86edf88a3452f16a
Jordan Ching <ChingJord@gmail.com>
2016-01-24 14:28:34 +1300

    member model and member table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124010358) do
+ActiveRecord::Schema.define(version: 20160124012624) do
+
+  create_table "members", force: true do |t|
+    t.string   "first_name",    default: "", null: false
+    t.string   "last_name",     default: "", null: false
+    t.string   "partner"
+    t.string   "last_assigned"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
