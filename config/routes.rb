96fc84af8b830576a908a4d21e16c1bc8da6aabd
Jordan Ching <ChingJord@gmail.com>
2016-01-24 14:25:50 +1300

    member controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'members/index'
+
+  get 'members/new'
+
+  get 'members/edit'
+
+  get 'members/show'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
