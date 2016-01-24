efcf8b05de411c33bc905d8bf104c23999ef5636
Jordan Ching <ChingJord@gmail.com>
2016-01-24 17:46:24 +1300

    secret santa matches

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,51 +1,64 @@
 class MembersController < ApplicationController
-	before_action :find_member, only: [:show, :edit, :update, :destroy]
+		before_action :find_member, only: [:show, :edit, :update, :destroy]
 
-  def index
-  	@members = Member.all
-  end
+	  def index
+		  	@members = Member.all
+		  	@participants = []
+		  	@members.each do |member|
+		  			@participants.push(member.first_name + ' ' + member.last_name)
+		  	end
+		  	@already_assigned = []
+		  	@assignees = []
+				@participants.each do |participant|
+						@leftovers = @participants - @already_assigned - [participant]
+						@assign = @leftovers.sample
+						@already_assigned.push(@assign)
+					  @assignees.push([participant, @assign])
+				end
 
-  def new
-		@member = Member.new
-	end
+	  end
 
-	def create
-		@member = Member.new(member_params)
-		if @member.save
-			redirect_to @member, notice: "Successfully created new member"
-		else
-			render "new"
+	  def new
+				@member = Member.new
 		end
-	end
 
-  def edit
-  end
-
-  def update
-		if @member.update(member_params)
-			redirect_to @member
-		else
-			render "edit"
+		def create
+				@member = Member.new(member_params)
+				if @member.save
+						redirect_to @member, notice: "Successfully created new member"
+				else
+						render "new"
+				end
 		end
-	end
 
-	def destroy
-		@member.destroy
-		redirect_to root_path, notice: "Successfully deleted member"
-	end
+	  def edit
+	  end
 
-  def show
-  end
+	  def update
+				if @member.update(member_params)
+						redirect_to @member
+				else
+						render "edit"
+				end
+		end
+
+		def destroy
+				@member.destroy
+				redirect_to root_path, notice: "Successfully deleted member"
+		end
+
+	  def show
+	  end
 
 private
 
-	def find_member
-		@member = Member.find(params[:id])
-	end
+		def find_member
+				@member = Member.find(params[:id])
+		end
 
-	def member_params
-		params.require(:member).permit(:first_name, :last_name, :partner, :last_assigned)
-	end
+		def member_params
+				params.require(:member).permit(:first_name, :last_name, :partner, :last_assigned)
+		end
 
 end
 
